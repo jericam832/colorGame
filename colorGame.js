7dad164bb5dbab0d1d9175fb6a11a3dc612cdf2d
Jerica Mitchell <jericam32@gmail.com>
2019-06-09 12:55:48 -0500

    Update colorGame.js

--- colorGame.js
+++ colorGame.js
@@ -62,7 +62,7 @@ function setupSquares(){
 			this.style.background = "none";
 			messageDisplay.style.color = "red";
 			messageDisplay.textContent = "Try Again!";
-			squares[i].style.boxShadow = "none";
+			this.style.boxShadow = "none";
 
 		}
 	});
