5c28a85a94d283056a85a66cbaf4315ce5b252c1
Jerica Mitchell <jericam32@gmail.com>
2019-06-09 11:32:58 -0500

    Update colorGame.js
    added modal

--- colorGame.js
+++ colorGame.js
@@ -132,4 +132,18 @@ function randomColor(){
 	return "rgb(" + r + ", " + g + ", " + b + ")";
 }
 
+//modal 
+var modal = document.querySelector(".modal");
+var trigger = document.querySelector(".trigger");
+var closeButton = document.querySelector(".close-button");
+
+function toggleModal() {
+    modal.classList.toggle("show-modal");
+}
+
+function windowOnClick(event) {
+    if (event.target === modal) {
+        toggleModal();
+    }
+}
 
